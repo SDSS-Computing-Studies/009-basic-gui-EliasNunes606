1a557bd9d1313d18041fea0c7f1865296adf0b71
Elias Nunes <eliasn510@deltalearns.ca>
2021-12-08 13:48:38 -0800

    1

--- task1.py
+++ task1.py
@@ -1,6 +1,19 @@
 import tkinter as tk 
 from tkinter import *
-from tkinter import ttk
+
 
 window = tk.Tk()
-window.geometry("200x400")
+entry1 = tk.Entry(window, borderwidth=3)
+entry1.grid(row=3, column = 1, columnspan=2)
+label1 = tk.Label(window,text="x")
+label1.grid(row=3, column = 6, columnspan=2)
+entry2 = tk.Entry(window, borderwidth=3)
+entry2.grid(row=3, column = 11, columnspan=2)
+label2 = tk.Label(window,text="=")
+label2.grid(row=3, column = 16, columnspan=2)
+entry3 = tk.Entry(window, borderwidth=3)
+entry3.grid(row=3, column = 21, columnspan=2)
+
+
+
+window.mainloop()
---
+++ task2.py
@@ -0,0 +1,18 @@
+import tkinter as tk 
+from tkinter import *
+
+window = tk.Tk()
+window.geometry("600x200")
+window.title("T-Town Veterinary Clinic Database")
+dogphoto = PhotoImage(file="dog.png")
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
