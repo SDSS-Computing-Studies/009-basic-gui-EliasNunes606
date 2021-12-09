092a75574af0e6ddc171b9e95d231bf631bffdba
Elias Nunes <eliasn510@deltalearns.ca>
2021-12-09 14:32:51 -0800

    1

--- task2.py
+++ task2.py
@@ -5,6 +5,10 @@ window = tk.Tk()
 window.geometry("600x200")
 window.title("T-Town Veterinary Clinic Database")
 dogphoto = PhotoImage(file="dog.png")
+label = tk.Label(window, image=dogphoto)
+
+
+
 
 
 
---
+++ task3.py
@@ -0,0 +1,9 @@
+import tkinter as tk 
+from tkinter import *
+
+
+
+
+
+
+window.mainloop()
