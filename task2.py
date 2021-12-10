5da92f2fb03b1963e92b73488d951af37ed341d9
Elias Nunes <eliasn510@deltalearns.ca>
2021-12-10 13:40:34 -0800

    1

--- task2.py
+++ task2.py
@@ -4,8 +4,13 @@ from tkinter import *
 window = tk.Tk()
 window.geometry("600x200")
 window.title("T-Town Veterinary Clinic Database")
+
 dogphoto = PhotoImage(file="dog.png")
 label = tk.Label(window, image=dogphoto)
+label.grid(row=1,column=1)
+
+label1 = tk.Label(window,text="Client Database")
+label1.grid(row=1,column=3)
 
 
 
